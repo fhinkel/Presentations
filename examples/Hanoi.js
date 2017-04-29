d3baf0123b9a5960dbe857aa32bb291eb4cbb298
Franziska Hinkelmann <franzih@chromium.org>
2017-04-29 12:09:05 +0200

    Add intro slides.

--- examples/Hanoi.js
+++ examples/Hanoi.js
@@ -1,17 +1,15 @@
 'use strict'
 
 function hanoi(n, src, aux, dest) {
-  if (n == 0) {
-    return;
-  }
+  if (n == 0) return;
   
   hanoi(n-1, src, dest, aux);
   console.log("Move disk " + n + " from " + src + " to " + dest + "."); 
   hanoi(n-1, aux, src, dest);
-
 }
 
 hanoi(4, 'initial stack', 'temporary stack', 'final stack');
-console.log("\n");
+
+
 
 
